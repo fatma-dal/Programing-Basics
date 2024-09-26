a7a35468551af9440c06b501203c6a8952c3c98c
fatma-dal <fatmadalhoumi123@gmail.com>
2024-09-26 14:21:30 +0200

    code1

---
+++ week1/Dalhoumi_Fatma_JSPredict.js
@@ -0,0 +1,3 @@
+//Predict 1 :"I was born in"1980
+//Predict 2 : Iwas born in1980
+//Predict 3 : summing Numbers!,num1 is:10,num2 is: 20,30
---
+++ week1/Dalhoumu_Fatma_Parameters.js
@@ -0,0 +1,11 @@
+//function greets the person by his name : thatis mean that the name change as a variable each time depending on the user so person it's a  parameter ;
+// day ; time to customize the fun
+//if person==CountDoco return "I'm coming for you Dooku"
+var name1= greet(person)
+if (name1="Count Dooku"){
+console.loge("I'm coming for you Dooku")
+}
+function greet(person,day,time){
+    console.log("Good day, "+person+"!"+day+time)
+    return person 
+}
